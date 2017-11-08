ad8d2df3c1643f080383192a413c56222947487d
Andrew Allen <w.andrewallen@gmail.com>
2017-11-08 14:40:25 -0600

    Assignment 2 List Chat Rooms: Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,9 +12,10 @@
                  controller: 'HomeCtrl as home',
                  templateUrl: '/templates/home.html'
              });
+
      }
 
      angular
          .module('blocChat', ['ui.router', 'firebase'])
-         .config(config); 
+         .config(config);
  })();
