390c5b50bcdd2ba88db152d0a093f813490830df
Andrew Allen <w.andrewallen@gmail.com>
2017-11-10 10:43:23 -0600

    Having trouble with chat name displaying

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,23 +1,20 @@
 (function() {
     function HomeCtrl(Room, $uibModal) {
       this.rooms = Room.all;
+      this.currentRoom = null;
 
+      this.setRoom = function(room) {
+        //this.currentRoom = room;
+        console.log("room");
+      };
 
       this.addRoom = function() {
         $uibModal.open({
           templateUrl: '/templates/modal.html',
           size: 'sm',
           controller: 'ModalCtrl as modal'
-        });
-      }
-
-
-
-      //this.deleteRoom = function() {
-        //console.log(Room.all.$);
-       //delete this.rooms;
-        //console.log(Room.remove);
-      //}
+        })
+      };
     }
 
     angular
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,7 +1,7 @@
 (function() {
   function Room($firebaseArray) {
     var Room = {};
-    var ref = firebase.database().ref().child("rooms");
+    var ref = firebase.database().ref().child('rooms');
     var rooms = $firebaseArray(ref);
 
     Room.all = rooms;
